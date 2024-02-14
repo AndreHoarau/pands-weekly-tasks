f0b77c791efd1f4967b4546b9e7a4a6c99a05a1e
Andre Hoarau <andgus95@gmail.com>
2024-02-14 15:09:05 +0000

    Tidy up as added to wrong folder

--- week03/labs3.1.1extrachallenge.py
+++
@@ -1,9 +0,0 @@
-# extra program question
-# author Andre Hoarau
-#message = 'I have eaten ' + 99 + ' burritos.'
-#print (message)
-# message creates an error as we are using the old format and the message needs to be all of one type in this case string
-message = f"I have eaten " + str(99) + " burritos."
-print (message)
-# eggs is a valid variable name and 100 is not as variables cannot begin with a number 
-# 3 functions to get integer floating point number or string version of a value are = int, fl, str.
--- week03/labs3.1.3 div.py
+++
@@ -1,7 +0,0 @@
-# a program that reads in two number and divdes the first one by the second and give the intefer result and the remainder
-# Author Andre Hoarau
-x = int(input("enter a first number "))
-y = int(input("enter a second number "))
-answer = int(x/y)
-remainder= x%y
-print( "{} divided by {} is {} with remainder {}" .format (x , y ,answer, remainder))

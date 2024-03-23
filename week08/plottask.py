feebbee1a9d71782ad15a00c863dd5294a375ce9
Andre Hoarau <andgus95@gmail.com>
2024-03-23 13:39:24 +0000

    one more comment to add

--- week08/plottask.py
+++ week08/plottask.py
@@ -36,6 +36,7 @@ plt.plot(x_values, y_values)
 plt.xlabel("Range")
 plt.ylabel("Frequency")
 # Let's add a title.
+# Source for making it look nicer: https://www.w3schools.com/python/matplotlib_labels.asp
 plt.title("Histogram 1000 Values Mean = 5, Std. Deviation = 2\n h(x)=x^3 (x 0-10 inclusive)")
 plt.show()
 
